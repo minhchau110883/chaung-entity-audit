f35999f017b31b319dd96aff327fae04b813a42e
Chau Ng <nguyen.min@ascendcorp.com>
2018-06-29 11:11:53 +0700

    initial commit

---
+++ src/main/java/com/chaung/entityaudit/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.chaung.entityaudit.repository;
+
+import com.chaung.entityaudit.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
